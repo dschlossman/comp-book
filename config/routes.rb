01d3006b1f19235497e284c27ac65b093c642f80
David Schlossman <dschlossman@gmail.com>
2014-07-13 19:20:13 -0500

    added Navigation Bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/about'
+  get 'pages/contact'
+  root 'pages#index'
   resources :reports
-
   resources :comps
-  root 'comps#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
