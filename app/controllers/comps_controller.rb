b92da0a465d33d3934a2c6808da6a80c70477081
David Schlossman <dschlossman@gmail.com>
2014-07-15 12:34:50 -0500

    updated priveleges

--- app/controllers/comps_controller.rb
+++ app/controllers/comps_controller.rb
@@ -1,6 +1,6 @@
 class CompsController < ApplicationController
   before_action :set_comp, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, :except => [:index, :show]
+  before_action :authenticate_user!, 
   
 
   # GET /comps
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   before_action :set_report, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, :except => [:index, :show]
+  before_action :authenticate_user!, :except => [:show]
 
   # GET /reports
   # GET /reports.json
