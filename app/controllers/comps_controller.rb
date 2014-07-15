ac6236df5761b8d7efc2c18a36b244f87d8bbd8f
David Schlossman <dschlossman@gmail.com>
2014-07-14 23:53:12 -0500

    added basic authentication to controllers and views

--- app/controllers/comps_controller.rb
+++ app/controllers/comps_controller.rb
@@ -1,5 +1,6 @@
 class CompsController < ApplicationController
   before_action :set_comp, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, :except => [:index, :show]
   
 
   # GET /comps
