4d65c946b5b98e1dafecf709c1fb62106d85d7ed
David Schlossman <dschlossman@gmail.com>
2014-07-15 18:50:53 -0500

    deleted a comma

--- app/controllers/comps_controller.rb
+++ app/controllers/comps_controller.rb
@@ -1,6 +1,6 @@
 class CompsController < ApplicationController
   before_action :set_comp, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, 
+  before_action :authenticate_user! 
   
 
   # GET /comps
